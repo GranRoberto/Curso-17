53fcca27fd18d95a795bb81c98a0680c2ab57c9e
GranRoberto <robertocariolo94@gmail.com>
2024-06-19 19:33:57 -0400

    a

--- Javascript/seccion2/index.js
+++ Javascript/seccion2/index.js
@@ -595,3 +595,83 @@ console.clear();
 
   Fecha de entrega: 20/06/2024
 */
+
+class AdivinaElNumero{
+
+  constructor(){
+    this.numero_aleatorio = Math.floor(Math.random() * 100) + 1;
+    this.vidas = 10;
+    this.historial = [];
+  }
+
+  adivinar(numero){
+    if(numero == this.numero_aleatorio){
+      alert('Adivinaste el numero');
+      return true;
+    } else if(numero > this.numero_aleatorio){
+      alert('El numero es menor');
+    } else {
+      alert('El numero es mayor');
+    }
+    this.historial.unshift(numero);
+    alert(this.historial);
+    this.vidas--;
+    if(this.vidas == 0){
+      alert('Se acabaron los intentos');
+      return true;
+    }
+    return false;
+  }
+
+  historial(){
+    alert(this.historial);
+  }
+}
+
+let adivina = new AdivinaElNumero();
+while(adivina.vidas > 0){
+  let numero = prompt('Ingresa un numero');
+  if(adivina.adivinar(numero)){
+    break;
+  }
+}
+
+
+/*dados*/
+
+class JuegoDados {
+  constructor(numero = 0) {
+    this.dado = 0;
+    this.numero = numero;
+  }
+
+  lanzarDados() {
+    this.dado = Math.floor(Math.random() * 6) + 1;
+    alert(`Dado ${this.numero}: ${this.dado}`);
+  }
+}
+
+let dado1 = new JuegoDados(1);
+let dado2 = new JuegoDados(2);
+let dado3 = new JuegoDados(3);
+
+while (true) {
+  dado1.lanzarDados();
+  dado2.lanzarDados();
+  dado3.lanzarDados();
+
+  if(dado1.dado == dado2.dado && dado2.dado == dado3.dado){
+    alert('Empate');
+  } else if(dado1.dado > dado2.dado && dado1.dado > dado3.dado){
+    alert('Gana dado 1');
+  } else if(dado2.dado > dado1.dado && dado2.dado > dado3.dado){
+    alert('Gana dado 2');
+  } else {
+    alert('Gana dado 3');
+  }
+
+  let continuar = confirm('Desea lanzar los dados de nuevo?');
+  if (!continuar) {
+    break;
+  }
+}
