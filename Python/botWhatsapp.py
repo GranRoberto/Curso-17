5442ba437bab6c555fc3cbdf2826bd394ff04a62
Roberto <robertocariolo94@gmail.com>
2024-09-09 19:40:30 -0400

    a

--- Python/botWhatsapp.py
+++ Python/botWhatsapp.py
@@ -1,7 +1,31 @@
 import pywhatkit as kit
+import time
+'''id = "ItSml9hdH7g4uC11pMkX80"
+mensaje = "Mensaje automatizado enviado con python"
 
+kit.sendwhatmsg_to_group(id, mensaje, 18, 55)'''
+
+kit.sendwhatmsg("+581316392", "Hi", 19 , 10)
+
+"""
 id = "ItSml9hdH7g4uC11pMkX80"
 mensaje = "Mensaje automatizado enviado con python"
 
-kit.sendwhatmsg_to_group(id, mensaje, 18, 54)
+kit.sendwhatmsg_to_group(id, mensaje, 18, 53)
+"""
 
+personas = [
+  {
+    "nombre": "Josue Centeno",
+    "telefono": "+584121201847"
+  },
+  {
+    "nombre": "Jesus Jimenez",
+    "telefono": "+584244284717"
+  }
+]
+
+for persona in personas:
+  mensaje = "Hola " + persona["nombre"] + ", este es un mensaje automatizado"
+  kit.sendwhatmsg_instantly(persona["telefono"], mensaje, 20, True)
+  time.sleep(10)
