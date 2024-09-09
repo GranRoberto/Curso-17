24515c62b6d1213d7aea60b5030b28890840abeb
Roberto <robertocariolo94@gmail.com>
2024-09-09 18:02:17 -0400

    a

---
+++ Typescript/index.js
@@ -0,0 +1,29 @@
+"use strict";
+// Variable en Javascript
+var hola = "Hola mundo";
+// Variable en TypeScript
+var holaMundo = "Hola mundo";
+var numero = 10;
+var booleano = true;
+var fecha = new Date();
+var cualquiera = "Hola mundo";
+// Arreglos
+var arreglo = ["Hola", "Mundo"];
+var arreglo2 = ["Hola", "Mundo"];
+// Tuplas
+var tupla = [1, "Hola"];
+// objetos
+var objeto = { nombre: "Juan", apellido: "Perez" };
+// Elemento HTML
+var elemento = document.body;
+// null y undefined
+var nulo = null;
+var indefinido = undefined;
+// Funciones
+function saludar(nombre) {
+    return "Hola " + nombre;
+}
+function saludar2(persona) {
+    //return "Hola " + persona.nombre + " " + persona.apellido;
+    return `Hola ${persona.nombre} ${persona.apellido}`;
+}
