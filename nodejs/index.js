0ca1726d02568e97855d3372f298524378c93e2c
Roberto <robertocariolo94@gmail.com>
2024-10-18 21:25:56 -0400

    a

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,27 @@
 import express, { json } from 'express'; //importamos express
+import mongoose from 'mongoose';
 const app = express(); // Crear el servidor ejecutando express
 const port = 3001;//crear un puerto
 app.use(json());// Middleware para parsear JSON
+
+// Utilizar mongoose para conectarnos a MongoDB
+connectDB().catch(err => console.log(err));
+
+async function connectDB() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/api');
+}
+
+// Definir un schema
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true, validate: /^[a-zA-Z0-9 ]+$/, unique: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true, min: 0 },
+  stock: { type: Number, required: true, min: 0 },
+  image: { type: String },
+  free_shipping: { type: Boolean, default: false },
+  shipping_cost: { type: Number, default: 0, min: 0 },
+});
+
 // Ruta principal
 app.get('/', (req, res) => {
   res.send('Bienvenido a la API con Express');
