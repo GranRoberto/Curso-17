82531c0bc284e70379189377cfe3b7c63e2426d5
Roberto <robertocariolo94@gmail.com>
2024-10-29 18:18:06 -0400

    a

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,6 +3,8 @@ config({ path: "./config/.env"}); //configuramos dotenv
 import express, { json } from "express"; //importamos express
 import mongoose from "mongoose";
 import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
 import routerProducts from "./routes/products.routes.js"; //importamos el archivo de rutas
 const app = express(); // Crear el servidor ejecutando express
 const port = process.env.PORT; //crear un puerto
@@ -10,13 +12,20 @@ app.use(json()); // Middleware para parsear JSON
 // Utilizar mongoose para conectarnos a MongoDB
 connectDB().catch((err) => console.log(err));
 
+// Definir __filename y __dirname para ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Configuración para servir la carpeta 'public' como recurso estático
+app.use("/public", express.static(path.join(__dirname, "public")));
+
 async function connectDB() {
   await mongoose.connect(process.env.DATABASE);
 }
 
 // Cors
 const corsOptions = {
-  origin: ["http://localhost:3000" , "http://localhost:5173"],
+  origin: ["http://localhost:3000","http://localhost:5173"],
   methods: ["GET", "POST", "PATCH", "DELETE"],
   optionsSuccessStatus: 200,
 };
@@ -29,6 +38,7 @@ app.use(routerProducts);
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
 
 
 /*
